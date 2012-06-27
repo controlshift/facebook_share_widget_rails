af820f22a028637727025b70d7a37f6b7f7be8a4
Nathan Woodhull <woodhull@gmail.com>
2012-06-27 17:23:18 -0400

    helper method.

--- app/helpers/facebook_share_widget/application_helper.rb
+++ app/helpers/facebook_share_widget/application_helper.rb
@@ -1,4 +1,8 @@
 module FacebookShareWidget
   module ApplicationHelper
+    def facebook_share_widget_helper
+      output = render partial: "/facebook_share_widget/facebook/widget"
+      output
+    end
   end
 end

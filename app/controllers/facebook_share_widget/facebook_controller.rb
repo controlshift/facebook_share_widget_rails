a01c8acae9ae30f1e6d264c7aa887572b09c7b20
Nathan Woodhull <woodhull@gmail.com>
2012-06-27 16:00:11 -0400

    log and mail exceptions instead of silently swallowing them.

--- app/controllers/facebook_share_widget/facebook_controller.rb
+++ app/controllers/facebook_share_widget/facebook_controller.rb
@@ -8,7 +8,7 @@ class FacebookShareWidget::FacebookController < FacebookShareWidget::Application
     begin
       render json: facebook_friends, status: :ok
     rescue Exception => ex
-      render json: { message: ex.message }, status: :internal_server_error
+      log_exception_and_render_as_json(ex)
     end
   end
   
@@ -17,7 +17,16 @@ class FacebookShareWidget::FacebookController < FacebookShareWidget::Application
       post(params[:post])
       render json: {}, status: :ok
     rescue Exception => ex
-      render json: { message: ex.message }, status: :internal_server_error
+      log_exception_and_render_as_json(ex)
     end
+  end
+  
+  private
+  
+  def log_exception_and_render_as_json(ex)
+    Rails.logger.warn ex.message
+    Rails.logger.warn ex.backtrace.join("\n")
+    ExceptionNotifier::Notifier.background_exception_notification(ex) if defined? ExceptionNotifier 
+    render json: { message: ex.message }, status: :internal_server_error
   end
 end

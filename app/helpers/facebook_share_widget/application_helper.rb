97740f622d953100dc895a022ce0b34dc55c747c
Nathan Woodhull <woodhull@gmail.com>
2012-06-27 19:46:45 -0400

    allow use of a share_url option to set the url to be shared.

--- app/helpers/facebook_share_widget/application_helper.rb
+++ app/helpers/facebook_share_widget/application_helper.rb
@@ -1,7 +1,8 @@
 module FacebookShareWidget
   module ApplicationHelper
-    def facebook_share_widget_helper
-      output = render partial: "/facebook_share_widget/facebook/widget"
+    def facebook_share_widget_helper(options = {})
+      options[:share_url] = "http://#{request.host}:#{request.port+request.fullpath}" if options[:share_url].blank?
+      output = render partial: "/facebook_share_widget/facebook/widget", locals: {options: options}
       output
     end
   end

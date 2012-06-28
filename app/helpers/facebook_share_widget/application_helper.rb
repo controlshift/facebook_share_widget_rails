45ac132682d6d309ab16a01363fd62b544d4a52c
Sean Ho <sean.ho@thoughtworks.com>
2012-06-28 17:25:40 +1000

    be able to specify post template (including link)

--- app/helpers/facebook_share_widget/application_helper.rb
+++ app/helpers/facebook_share_widget/application_helper.rb
@@ -1,8 +1,9 @@
 module FacebookShareWidget
   module ApplicationHelper
     def facebook_share_widget_helper(options = {})
-      options[:share_url] = "http://#{request.host}:#{request.port+request.fullpath}" if options[:share_url].blank?
-      output = render partial: "/facebook_share_widget/facebook/widget", locals: {options: options}
+      options[:template] = {} if options[:template].nil?
+      options[:template][:link] = "http://#{request.host}:#{request.port.to_s+request.fullpath}" if options[:template][:link].blank?
+      output = render partial: "/facebook_share_widget/facebook/widget", locals: { options: options }
       output
     end
   end

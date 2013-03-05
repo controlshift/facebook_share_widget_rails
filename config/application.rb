e37bf01b3126e6c75738527e86e711ec15a31dd2
Rohit Garg <rohitgarg19@gmail.com>
2013-03-05 10:33:42 +0530

    Rohit fixed asset pipeline path

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 module FacebookShareWidget
 	class Application < Rails::Application
-		config.assets.precompile += ['facebook_share.js', 'facebook_share_widget/facebook_friend_view.js']
+		config.assets.precompile += ['facebook_share_widget/facebook_friend_view.js']
 	end
 end

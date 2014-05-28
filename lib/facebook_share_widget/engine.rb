f7b64e71b6674fd1f9cb8fb83c0812292f3f3936
Nathan Woodhull <woodhull@gmail.com>
2014-05-28 16:16:57 -0700

    Revert "turn off compression to help with debugging in production."
    This reverts commit d3bc0d0eb5b44688b50a02475ae629f1a6eaa04c.

--- lib/facebook_share_widget/engine.rb
+++ lib/facebook_share_widget/engine.rb
@@ -7,10 +7,5 @@ require_relative '../../app/actions/facebook_share_widget/personal_data'
 module FacebookShareWidget
   class Engine < ::Rails::Engine
     isolate_namespace FacebookShareWidget
-
-    initializer "facebook_share_widget.assets.precompile" do |app|
-      app.config.assets.compress = false
-    end
-
   end
 end
